7930e0627c0f7cba7da40fab14ea25f2b9af9b0b
Greg Caporaso <gregcaporaso@gmail.com>
2013-07-29 11:29:18 -0700

    updated version for first release

--- qcli/__init__.py
+++ qcli/__init__.py
@@ -2,9 +2,13 @@
 
 __author__ = "The BiPy Development Team"
 __copyright__ = "Copyright 2013, The BiPy Project"
-__credits__ = ["Rob Knight", "Greg Caporaso", ] 
+__credits__ = ["Rob Knight", 
+               "Greg Caporaso", 
+               "Gavin Huttley", 
+               "Daniel McDonald",
+               "Jai Ram Rideout"] 
 __license__ = "GPL"
-__version__ = "0.0.0-dev"
+__version__ = "0.1.0"
 __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 
