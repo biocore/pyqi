7c16d74d54fa7e610fe6e9e284b3fc0354ead4b6
Greg Caporaso <gregcaporaso@gmail.com>
2013-08-02 11:13:15 -0700

    better name, plus added file that should have been added in last commit

--- qcli/qiime_backports/scripts/filter_samples_from_otu_table.py
+++ qcli/qiime_backports/scripts/filter_samples_from_otu_table.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 from sys import argv
-from qcli.qiime_backports.qiime.util import old_to_new_command
+from qcli.qiime_backports.qiime.util import old_to_new_qiime_command
 
-old_to_new_command(argv)
+old_to_new_qiime_command(argv)
