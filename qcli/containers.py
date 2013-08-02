1b811204a85b0a8700d1df848d077660404ad323
Daniel McDonald <wasade@gmail.com>
2013-08-01 19:45:33 -0600

    added license, attribution, etc

--- qcli/containers.py
+++ qcli/containers.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013, The BiPy Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+__author__ = "Daniel McDonald"
+__copyright__ = "Copyright 2013, The QIIME Project"
+__credits__ = ["Greg Caporaso", "Daniel McDonald", "Doug Wendel",
+               "Jai Ram Rideout"]
+__license__ = "BSD"
+__version__ = "0.1.0-dev"
+__maintainer__ = "Daniel McDonald"
+__email__ = "mcdonadt@colorado.edu"
+
 class ContainerError(Exception):
     pass
  
