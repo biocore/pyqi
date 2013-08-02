c044af8ef36c9071d3d10f4f8ef866d3fcc661d7
Daniel McDonald <wasade@gmail.com>
2013-08-02 13:09:39 -0600

    added qcli driver, and a make_command method

--- qcli/container.py
+++ qcli/container.py
@@ -65,105 +65,148 @@ class Passthrough(object):
         self._load_if_needed()
         
         return hasattr(self._object, attr)
- 
-class DelayIO(Passthrough):
-    """Contain an object and issue IO when an object attribute is requested"""
+
+class PassthroughIO(Passthrough):
     _reserved = set(['_reserved', 'TypeName', '_reader', '_writer',
                      '_object', 'InPath', 'OutPath','read', 'write',
                      '_load_if_needed', 'Info'])
-    TypeName = "DelayIO"
-    _object = None
-    InPath = None
-    OutPath = None
-    Info = None
- 
+    TypeName = "PassthroughIO"
+     
     def __init__(self, *args, **kwargs):
-        super(DelayIO, self).__init__(*args, **kwargs)
+        super(PassthroughIO, self).__init__(*args, **kwargs)
         
         if 'Object' in kwargs:
-            super(DelayIO, self).__setattr__('_object',  kwargs['Object'])
+            super(PassthroughIO, self).__setattr__('_object',  kwargs['Object'])
+        else:
+            self._object = None
+
         if 'InPath' in kwargs:
-            super(DelayIO, self).__setattr__('InPath',  kwargs['InPath'])
+            super(PassthroughIO, self).__setattr__('InPath',  kwargs['InPath'])
+        else:
+            self.InPath = None
+
         if 'OutPath' in kwargs:
-            super(DelayIO, self).__setattr__('OutPath',  kwargs['OutPath'])
- 
+            super(PassthroughIO, self).__setattr__('OutPath',  kwargs['OutPath'])
+        else:
+            self.OutPath = None
+
     def _load_if_needed(self):
         """Load if the object has not already been loaded"""
         if self._object is None:
             if self.InPath is not None:
-                self._object = self._reader(self.InPath)
+                self._object = self._reader(self, self.InPath)
             else:
                 raise CannotReadError("No object and InPath is None!")
     
-    def write(self):
-        """Attempted to write"""
-        if self._object is not None:
-            if self.OutPath is None:
-                raise CannotWriteError("OutPath is None!")
-            self._writer(self.OutPath)
- 
     def read(self):
         """Attempt to read"""
         if self._object is None:
             if self.InPath is None:
                 raise CannotReadError("InPath is None!")
-            self._object = self._reader(self.InPath)
-
-class ImmediateRead(Passthrough):
-    """Issue an immediate read on construction"""
-    _reserved = set(['_reserved', 'TypeName', '_reader', '_writer',
-                     '_object', 'InPath', 'OutPath','read', 'write',
-                     '_load_if_needed', 'Info'])
-    TypeName = "ImmediateRead"
-    _object = None
-    InPath = None
-    OutPath = None
-    Info = None
- 
-    def __init__(self, *args, **kwargs):
-        super(ImmediateRead, self).__init__(*args, **kwargs)
-        
-        if 'Object' in kwargs:
-            super(ImmediateRead, self).__setattr__('_object',  kwargs['Object'])
-        if 'InPath' in kwargs:
-            super(ImmediateRead, self).__setattr__('InPath',  kwargs['InPath'])
-        if 'OutPath' in kwargs:
-            super(ImmediateRead, self).__setattr__('OutPath',  kwargs['OutPath'])
-        
-        self.read()
-
-    def read(self):
-        """Attempt to read"""
-        if self._object is None:
-            if self.InPath is None:
-                raise CannotReadError("InPath is None!")
-            self._object = self._reader(self.InPath)
-
-class ImmediateWrite(Passthrough):
-    _reserved = set(['_reserved', 'TypeName', '_reader', '_writer',
-                     '_object', 'InPath', 'OutPath','read', 'write',
-                     '_load_if_needed', 'Info'])
-    TypeName = "ImmediateWrite"
-    _object = None
-    InPath = None
-    OutPath = None
-    Info = None
- 
-    def __init__(self, *args, **kwargs):
-        super(ImmediateWrite, self).__init__(*args, **kwargs)
-        
-        if 'Object' in kwargs:
-            super(ImmediateWrite, self).__setattr__('_object',  kwargs['Object'])
-        if 'InPath' in kwargs:
-            super(ImmediateWrite, self).__setattr__('InPath',  kwargs['InPath'])
-        if 'OutPath' in kwargs:
-            super(ImmediateWrite, self).__setattr__('OutPath',  kwargs['OutPath'])
-        
-        self.write()
+            self._object = self._reader(self, self.InPath)
 
     def write(self):
         """Attempt to write"""
+        if self._object is None:
+            self._read()
         if self._object is not None:
             if self.OutPath is None:
                 raise CannotWriteError("OutPath is None!")
-            self._writer(self.OutPath)
+            self._writer(self, self.OutPath)
+            self._object = None
+
+class PassthroughRead(PassthroughIO):
+    def __init__(self, *args, **kwargs):
+        if 'reader' in kwargs:
+            super(PassthroughRead, self).__setattr__('_reader', kwargs['reader'])
+        else:
+            raise ContainerError("A reader is required!")
+        super(PassthroughRead, self).__init__(*args, **kwargs)
+
+class PassthroughWrite(PassthroughIO):
+    def __init__(self, *args, **kwargs):
+        if 'writer' in kwargs:
+            super(PassthroughWrite, self).__setattr__('_reader', kwargs['writer'])
+        else:
+            raise ContainerError("A writer is required!")
+        super(PassthroughWrite, self).__init__(*args, **kwargs)
+
+class DelayRead(PassthroughRead):
+    """Contain an object and issue IO when an object attribute is requested"""
+    TypeName = "DelayRead"
+ 
+class DelayWrite(PassthroughWrite):
+    """Contain an object and issue IO with the container is no more"""
+    TypeName = "DelayWrite"
+     
+    def __del__(self):
+        self.write()
+
+class ImmediateRead(PassthroughRead):
+    """Issue an immediate read on construction"""
+    TypeName = "ImmediateRead"
+ 
+    def __init__(self, *args, **kwargs):
+        super(ImmediateRead, self).__init__(*args, **kwargs)
+        self.read() 
+
+class ImmediateWrite(PassthroughWrite):
+    TypeName = "ImmediateWrite"
+ 
+    def __init__(self, *args, **kwargs):
+        super(ImmediateWrite, self).__init__(*args, **kwargs)
+        self.write()    
+
+def default_write_str(obj, path):
+    f = open(path,'w')
+    f.write(str(obj._object))
+    f.close()
+
+def default_read_str(obj, path):
+    f = open(path)
+    return f.read()
+
+def default_write_object(obj, path):
+    f = open(path, 'w')
+    f.write(repr(obj._object))
+    f.close()
+
+def default_read_object(obj, path):
+    f = open(path)
+    return f.read() # eval isn't safe...
+
+IOType = {'ImmediateRead':ImmediateRead,
+            'ImmediateWrite':ImmediateWrite,
+            'DelayRead':DelayRead,
+            'DelayWrite':DelayWrite}
+
+IOLookup = {str:(default_read_str, default_write_str)}
+
+def WithIO(obj, IO_type=None, IO_lookup=None, **kwargs):
+    if IO_type is None:
+        raise ContainerError("IO_type is required!")
+    
+    if IO_type not in IOType:
+        raise ContainerError("Unknown IO_type: %s" % IO_type)
+    
+    if kwargs is None:
+        kwargs = {}
+
+    if IO_lookup is None:
+        IO_lookup = IOLookup
+
+    obj_type = obj.__class__
+    kwargs['Object'] = obj
+
+    if obj_type in IO_lookup:
+        reader, writer = IO_lookup[obj_type]
+    else:
+        reader, writer = default_read_object, default_write_object
+    
+    kwargs['reader'] = reader
+    kwargs['writer'] = writer
+
+    return IOType[IO_type](**kwargs)
+
+def WithoutIO(obj, **kwargs):
+    return obj
