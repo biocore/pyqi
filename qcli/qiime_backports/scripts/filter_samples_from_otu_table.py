7d2c4d04727bebe0d79ddc135b420b4728e97925
Greg Caporaso <gregcaporaso@gmail.com>
2013-08-02 11:03:06 -0700

    generalized functionality

--- qcli/qiime_backports/scripts/filter_samples_from_otu_table.py
+++ qcli/qiime_backports/scripts/filter_samples_from_otu_table.py
@@ -5,17 +5,19 @@ from os.path import split, splitext
 from qcli.util import qcli_system_call
 from qcli.log import StdErrLogger
 
-logger = StdErrLogger()
+def old_to_new_command(local_argv):
+    logger = StdErrLogger()
 
-cmd_name = splitext(split(argv[0])[1])[0]
-base_cmd = "qiime %s" % cmd_name 
-command = '%s %s' % (base_cmd,' '.join(argv[1:]))
+    cmd_name = splitext(split(local_argv[0])[1])[0]
+    base_cmd = "qiime %s" % cmd_name 
+    command = '%s %s' % (base_cmd,' '.join(local_argv[1:]))
 
-logger.info("This is a new-style QIIME script. You should now call it with: %s" % base_cmd)
-logger.info("Calling: %s " % command)
+    logger.info("This is a new-style QIIME script. You should now call it with: %s" % base_cmd)
+    logger.info("Calling: %s " % command)
 
-result_stdout, result_stderr, result_retval = qcli_system_call(command)
+    result_stdout, result_stderr, result_retval = qcli_system_call(command)
 
-stdout.write(result_stdout)
-stderr.write(result_stderr)
+    stdout.write(result_stdout)
+    stderr.write(result_stderr)
 
+old_to_new_command(argv)
