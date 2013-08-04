037726b96680ab29f0457d56e7b493b55cf4e172
Doug Wendel <=>
2013-08-04 05:01:39 -0600

    fixes to make web code run again, looks a little prettier.

--- pyqi/qiime_backports/bin/htbin/commands.py
+++ pyqi/qiime_backports/bin/htbin/commands.py
@@ -9,6 +9,7 @@ __version__ = "0.1.0-dev"
 __maintainer__ = "Jai Ram Rideout"
 __email__ = "jai.rideout@gmail.com"
 
+import traceback
 from pyqi.interface.cli import clmain, cli
 import pyqi.qiime_backports.qiime.cli as qiime_cli
 from pyqi.qiime_backports.bin.htbin.util import (get_cmd_cfg,
@@ -16,15 +17,18 @@ from pyqi.qiime_backports.bin.htbin.util import (get_cmd_cfg,
                                                  format_page_footer)
 
 print format_page_header()
-print "<p>I know about these QIIME commands:</p>"
-print "<table border='1'>"
+print "<h2>List of QIIME commands:</h2>"
+print "<table>"
 
 for c in sorted(qiime_cli.__all__):
-    cmd_cfg = get_cmd_cfg(c)
-    desc = cmd_cfg.CommandConstructor.BriefDescription
-    print "<tr>"
-    print "<td><a href='run_command.py?command={0}'>{0}</td><td>{1}</td>".format(c, desc)
-    print "</tr>"
+	try:
+	    cmd_cfg = get_cmd_cfg(c)
+	    desc = cmd_cfg.CommandConstructor.BriefDescription
+	    print "<tr>"
+	    print "<td><a href='run_command.py?command={0}'>{0}</td><td>{1}</td>".format(c, desc)
+	    print "</tr>"
+	except Exception, e:
+		print '<pre>{0}</pre>'.format(traceback.format_exc())
 
 print "</table>"
 print format_page_footer()
--- pyqi/qiime_backports/bin/htbin/run_command.py
+++ pyqi/qiime_backports/bin/htbin/run_command.py
@@ -23,16 +23,18 @@ brief_desc = cmd.BriefDescription
 long_desc = cmd.LongDescription
  
 print format_page_header()
-print "<h2>%s</h2>" % cmd_name
-print brief_desc
+print "<h2>Script Name: %s</h2>" % cmd_name
+print "Description: {0}".format(brief_desc)
 print "<br/>"
 print long_desc
+print "<br/>"
 print "<form method='POST' action='command_results.py'>"
 print "<table border='1'>"
 for parameter in cmd.Parameters:
     print "<tr><td>{0}</td><td><input type='text' value='{1}' id='qiime_parameter_{0}' name='qiime_parameter_{0}' /></td></tr>".format(parameter.Name, parameter.Default)
 print "</table>"
 print "<input type='hidden' name='{0}' id='{0}' value='{1}' />".format('command', cmd_name)
-print "<input type='submit' value='Run!' />"
+print "<br/>"
+print "<input type='submit' value='Run Command!' />"
 print "</form>"
 print format_page_footer()
