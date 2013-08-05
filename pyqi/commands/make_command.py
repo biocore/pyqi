c30f6126d7890610ac17f36680f8d88e1f24bbb0
Daniel McDonald <wasade@gmail.com>
2013-08-05 08:55:28 -0600

    make_command updated

--- pyqi/commands/make_command.py
+++ pyqi/commands/make_command.py
@@ -17,8 +17,7 @@ __version__ = "0.1.0-dev"
 __maintainer__ = "Daniel McDonald"
 __email__ = "mcdonadt@colorado.edu"
 
-from pyqi.core.container import WithIO
-from pyqi.core.command import Command, Parameter
+from pyqi.core.command import Command, Parameter, ParameterCollection
 
 header = """#!/usr/bin/env python
 
@@ -38,55 +37,61 @@ __email__ = "%(email)s"
 command_format = """class %s(Command):
     BriefDescription = "FILL IN A 1 SENTENCE DESCRIPTION"
     LongDescription = "GO INTO MORE DETAIL"
-
+    Parameters = ParameterCollection([
+        Parameter(Name='foo',Required=True,DataType=str,
+                  Help='some required parameter'),
+        Parameter(Name='bar',Required=False,DataType=int,
+                  Help='some optional parameter',Default=1)
+        ])
+        
     def run(self, **kwargs):
         # EXAMPLE:
         # return {'result_1': kwargs['foo'] * kwargs['bar'],
         #         'result_2': "Some output bits"}
         raise NotImplementedError("You must define this method")
 
-    def _get_parameters(self):
-        # EXAMPLE:
-        # return [Parameter(Name='foo',Required=True,Type=str,
-        #                   Help='some required parameter'),
-        #         Parameter(Name='bar',Required=False,Type=int,
-        #                   Help='some optional parameter',Default=1)]
-        raise NotImplementedError("You must define this method")
-
 CommandConstructor = %s
 """
 
-class CLIStub(Command):
+class MakeCommand(Command):
     BriefDescription = "Construct a stringified stubbed out ``Command`` object"
     LongDescription = """This method will is intended to construct the basics of a ``Command`` object to so that a developer can dive straight into the fun bits"""
-
+    Parameters = ParameterCollection([
+        Parameter(Name='name',Required=True,DataType=str,
+                  Help='the name of the ``Command``'), 
+        Parameter(Name='email',Required=True,DataType=str,
+                  Help='maintainer email address'),
+        Parameter(Name='author',Required=True,DataType=str,
+                  Help='the function author'),
+        Parameter(Name='license',Required=True,DataType=str,
+                  Help='the license for the function'),
+        Parameter(Name='copyright',Required=True,DataType=str,
+                  Help='the function copyright'),
+        Parameter(Name='func_version',Required=True,DataType=str,
+                  Help='the function version'),
+### Default is not honored right now
+        Parameter(Name='credits',Required=False,DataType=str,Default='',
+                  Help='comma separated list of other authors')
+        ])
+    
     def run(self, **kwargs):
         # build a string formatting dictionary for the file header
-        head = dict([(k,v) for k,v in kwargs.items() if k not in ['name']])
+        head = {}
+        head['email']        = kwargs['email']
+        head['author']       = kwargs['author']
+        head['license']      = kwargs['license']
+        head['copyright']    = kwargs['copyright']
+        head['func_version'] = kwargs['func_version']
         
-        if 'credits' in head:
+        if 'credits' in kwargs:
             f = lambda x: '"%s"' % x
             head['credits'] = ', '.join(map(f, head['credits'].split(',')))
+        else:
+            head['credits'] = ''
 
         result_lines = [header % head]
         result_lines.append(command_format % (kwargs['name'], kwargs['name']))
 
         return {'result':''.join(result_lines)}
 
-    def _get_parameters(self):
-        return [Parameter(Name='name',Required=True,Type=str,
-                          Help='the name of the ``Command``'), 
-                Parameter(Name='email',Required=True,Type=str,
-                          Help='maintainer email address'),
-                Parameter(Name='author',Required=True,Type=str,
-                          Help='the function author'),
-                Parameter(Name='license',Required=True,Type=str,
-                          Help='the license for the function'),
-                Parameter(Name='copyright',Required=True,Type=str,
-                          Help='the function copyright'),
-                Parameter(Name='func_version',Required=True,Type=str,
-                          Help='the function version'),
-                Parameter(Name='credits',Required=False,Type=str,Default='',
-                          Help='comma separated list of other authors')]
-    
-CommandConstructor = CLIStub
+CommandConstructor = MakeCommand
