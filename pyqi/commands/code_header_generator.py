b221821deb3bdfd47f5620dc43c5983034ad7712
Adam Robbins-Pianka <adam.robbinspianka@colorado.edu>
2013-12-02 22:06:43 -0700

    Modify header gen, make all arguments optional

--- pyqi/commands/code_header_generator.py
+++ pyqi/commands/code_header_generator.py
@@ -14,18 +14,6 @@ __credits__ = ["Jai Ram Rideout", "Daniel McDonald"]
 from pyqi.core.command import (Command, CommandIn, CommandOut, 
     ParameterCollection)
 
-header_format = """#!/usr/bin/env python
-from __future__ import division
-
-__author__ = "%(author)s"
-__copyright__ = "%(copyright)s"
-__credits__ = [%(credits)s]
-__license__ = "%(license)s"
-__version__ = "%(version)s"
-__maintainer__ = "%(author)s"
-__email__ = "%(email)s"
-"""
-
 class CodeHeaderGenerator(Command):
     BriefDescription = "Generate header code for use in a Python file"
     LongDescription = ("Generate valid Python code containing header "
@@ -35,16 +23,16 @@ class CodeHeaderGenerator(Command):
 
     CommandIns = ParameterCollection([
         CommandIn(Name='author', DataType=str,
-                  Description='author/maintainer name', Required=True),
+                  Description='author/maintainer name', Required=False),
         CommandIn(Name='email', DataType=str,
-                  Description='maintainer email address', Required=True),
+                  Description='maintainer email address', Required=False),
         CommandIn(Name='license', DataType=str,
-                  Description='license (e.g., BSD)', Required=True),
+                  Description='license (e.g., BSD)', Required=False),
         CommandIn(Name='copyright', DataType=str,
                   Description='copyright (e.g., Copyright 2013, The pyqi '
-                              'project)', Required=True),
+                              'project)', Required=False),
         CommandIn(Name='version', DataType=str,
-                  Description='version (e.g., 0.1)', Required=True),
+                  Description='version (e.g., 0.1)', Required=False),
         CommandIn(Name='credits', DataType=list,
                   Description='list of other authors',
                   Required=False, Default=None)
@@ -57,20 +45,46 @@ class CodeHeaderGenerator(Command):
     def run(self, **kwargs):
         # Build a string formatting dictionary for the file header.
         head = {}
-        head['author']    = kwargs['author']
-        head['email']     = kwargs['email']
-        head['license']   = kwargs['license']
-        head['copyright'] = kwargs['copyright']
-        head['version']   = kwargs['version']
 
-        # Credits always includes author.
+        fields = ['author', 'copyright', 'credits', 'license', 'version',
+            'email']
+        
+        for field in fields:
+            head[field] = kwargs.get(field, None)
+
+        # Credits always includes author. Note that even if neither author nor
+        # credits is passed, credits will be an empty list and will be written
+        # out
         credits = [head['author']]
-        if kwargs['credits']:
+        if kwargs['credits'] is not None:
             credits.extend(kwargs['credits'])
-
+        credits = filter(lambda x: x is not None, credits)
         f = lambda x: '"%s"' % x
         head['credits'] = ', '.join(map(f, credits))
 
-        return {'result': (header_format % head).split('\n')}
+        header_lines = []
+        header_lines.append("#!/usr/bin/env python")
+        header_lines.append("from __future__ import division")
+        header_lines.append("")
+
+        if head['author'] is not None:
+            header_lines.append('__author__ = "%s"' % head['author'])
+        if head['copyright'] is not None:
+            header_lines.append('__copyright__ = "%s"' % head['copyright'])
+        if head['credits'] is not None:
+            header_lines.append('__credits__ = [%s]' % head['credits'])
+        if head['license'] is not None:
+            header_lines.append('__license__ = "%s"' % head['license'])
+        if head['version'] is not None:
+            header_lines.append('__version__ = "%s"' % head['version'])
+        if head['author'] is not None:
+            header_lines.append('__maintainer__ = "%s"' % head['author'])
+        if head['email'] is not None:
+            header_lines.append('__email__ = "%s"' % head['email'])
+
+        header_lines.append("")
+        header_format = ''.join(header_lines)
+
+        return {'result': header_lines}
 
 CommandConstructor = CodeHeaderGenerator
